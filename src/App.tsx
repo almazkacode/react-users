4eece96df7d235ae7f39876084640166448f4804
Olga Smirnova <almazkacode@gmail.com>
2025-04-08 14:01:01 +0300

    feat: add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import 'normalize.css';
-import './App.scss';
+// import { Routes, Route, Navigate } from 'react-router-dom';
+import { Header } from './components/layout/Header/Header';
+// import { Home } from './pages/Home/Home';
+// import { Profile } from './pages/Profile/Profile';
 
 function App() {
-  return <></>;
+  return <Header />;
 }
 
 export default App;
